d28a653c3e811e8c228eb4b0ee417e2c43fdd8bd
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-03 14:37:37 -0700

    Add project files.

---
+++ BookUserAccount/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookUserAccount
+{
+    /// <summary>
+    /// Represents a individual user's account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Creates an account with the specific user, and has hours and books of 0
+        /// </summary>
+        /// <param name="userName">The user's name who owns the account</param>
+        public Account(string userName)
+        {
+            UserName = userName;
+        }
+        /// <summary>
+        /// The user's name of the account.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// The amount of books that the user has read.
+        /// </summary>
+        public int Books { get; private set; }
+
+        /// <summary>
+        /// The amount of hours that the user spent reading.
+        /// </summary>
+        public int Hours { get; private set; }
+
+        /// <summary>
+        /// Add a specified amount of books to the user's account
+        /// </summary>
+        /// <param name="amtbooks">The positive amount to 
+        /// number of books read</param>
+        /// <returns>The new amount of books read after deposit</returns>
+        public int DepositBooks(int amtbooks)
+        {
+            Books += amtbooks;
+            return Books;
+        }
+
+        /// <summary>
+        /// Add a specified amount of hours spent reading to the user's account
+        /// </summary>
+        /// <param name="amthours">The positive amount to 
+        /// number of hours spent reading</param>
+        /// <returns>The new amount of hours spent reading</returns>
+        public int DepositHours(int amthours)
+        {
+            Hours += amthours;
+            return Hours;
+        }
+    }
+}
---
+++ BookUserAccountTests/AccountTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookUserAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookUserAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void DepositBooks_APositiveAmount_AddToBooks()
+        {
+            Account acc = new Account("J Leon");
+            acc.DepositBooks(2);
+
+            Assert.AreEqual(2, acc.Books);
+        }
+
+        [TestMethod()]
+        public void DepositHours_APositiveAmount_AddToHours()
+        {
+            Account acc = new Account("B Wilbur");
+            acc.DepositHours(3);
+
+            Assert.AreEqual(3, acc.Hours);
+        }
+    }
+}
