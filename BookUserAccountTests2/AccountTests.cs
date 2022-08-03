144a2eaebacaa17ac589e6eacf2f8580d0ef493a
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-03 14:47:00 -0700

    Added Deposit Unit tests

---
+++ BookUserAccountTests2/AccountTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookUserAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookUserAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void DepositBooks_APositiveAmount_AddToBooks()
+        {
+            Account acc = new("J Leon");
+            acc.DepositBooks(2);
+
+            Assert.AreEqual(2, acc.Books);
+        }
+
+        [TestMethod()]
+        public void DepositBooks_APositiveAmount_ReturnsUpdatedBooks()
+        {
+            // AAA - Arrange Act Assert
+            // Arrange 
+            Account acc = new("H Jakons");
+            int depositBooks = 3;
+            int expectedReturn = 3;
+
+            // Act
+            int returnValue = acc.DepositBooks(depositBooks);
+
+            // Assert
+            Assert.AreEqual(expectedReturn, returnValue);
+        }
+
+        [TestMethod()]
+        public void DepositHours_APositiveAmount_AddToHours()
+        {
+            Account acc = new("B Wilbur");
+            acc.DepositHours(3);
+
+            Assert.AreEqual(3, acc.Hours);
+        }
+
+        [TestMethod()]
+        public void DepositHours_APositiveAmount_ReturnsUpdatedBooks()
+        {
+            // AAA - Arrange Act Assert
+            // Arrange 
+            Account acc = new("F Flores");
+            int depositHours = 1;
+            int expectedReturn = 1;
+
+            // Act
+            int returnValue = acc.DepositHours(depositHours);
+
+            // Assert
+            Assert.AreEqual(expectedReturn, returnValue);
+        }
+    }
+}
